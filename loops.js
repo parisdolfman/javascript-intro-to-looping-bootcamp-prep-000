36fa1a4fb4bd68b519454b4b85ab5bcc12a57f09
Paris Dolfman <parisdolfman@gmail.com>
2018-12-12 21:08:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,13 @@ function whileLoop(n) {
   }
 return "done"
 }
+
+function doWhileLoop(array) {
+  var i = 0;
+  function incrementVariable(){
+    i = i + 1;
+  }
+  do {
+    array.pop;
+  } while (array.length > 0);
+}
